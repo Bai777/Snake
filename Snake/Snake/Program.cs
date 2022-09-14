33ecd6fc6e89a09d59598ea05f5dbd96aad68f28
bai_777 <bai_777@mail.ru>
2022-09-14 19:43:28 +0300

    lesson-2

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int x = 1;
+int y = 3;
+char symbol = '*';
+
+Draw(x, y, symbol);
 Console.ReadLine();
+
+void Draw(int x, int y, char symbol)
+{
+    Console.SetCursorPosition(x, y);
+    Console.Write(symbol);
+}
